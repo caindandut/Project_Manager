bc4b63fc6a116254a5c0d17a20f18c4a6e4ec64a
ledangkhanhdut <khanhdangabc2@gmail.com>
2026-06-08 12:34:23 +0700

    fix: revert workspace creator role back to ADMIN

--- backend/src/modules/workspace/workspace.repository.ts
+++ backend/src/modules/workspace/workspace.repository.ts
@@ -178,7 +178,7 @@ export class WorkspaceRepository extends BaseRepository<
         members: {
           create: {
             userId: ownerId,
-            role: WorkspaceRole.OWNER,
+            role: WorkspaceRole.ADMIN,
           },
         },
       },
